d64ad21a4c35219950c906f0c84fc9af92311fb5
Kareem Aboelnaga <79963866+KAboelnaga@users.noreply.github.com>
2026-09-20 19:16:04 +0300

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        hashmap = {}
+        if len(s) != len(t):
+            return False
+        for char in s:
+            hashmap[char] = hashmap.get(char, 0) + 1
+        for char in t:
+            if char in hashmap and hashmap[char] > 0:
+                hashmap[char] -= 1
+            else:
+                return False
+        # for char in hashmap:
+        #     if hashmap[char] > 0:
+        #         return False
+        return True
+
+        
